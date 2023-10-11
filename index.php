a186fc1bfb2e92a1bda3ed03444bbbc3835e5dae
LessRaHu <zthlssl@gmail.com>
2023-10-11 07:47:23 -0500

    less

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include 'template/header.php' ?>
+
+<?php
+    include_once "model/conexion.php";
+    $sentencia = $bd->query("select * from reservas");
+    $reservas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<div class="container w-100 mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-gray-900 text-white">
+                    <h2 class="card-title text-center font-weight-bold text-dark">SISTEMA ADMINISTRATIVO</h2>
+                </div>
+                <div class="header-button">
+                    <a href="nuevo.php">
+                        <button type="button" class="btn btn-primary" style="background-color: #333;">Agregar nuevo Cliente</button>
+                    </a>
+                </div>
+                <div class="p-4">
+                    <div class="table-responsive">
+                        <table class="table table-dark table-striped w-100 max-width-lg">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Nombres</th>
+                                    <th scope="col">Apellidos</th>
+                                    <th scope="col">DNI</th>
+                                    <th scope="col">Tipo de Habitacion</th>
+                                    <th scope="col">Fecha de Reserva</th>
+                                    <th scope="col">Ingreso</th>
+                                    <th scope="col">Salida</th>
+                                    <th scope="col">Celular</th>
+                                    <th scope="col">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($reservas as $dato): ?>
+                                    <tr>
+                                        <td scope="row"><?php echo $dato->id; ?></td>
+                                        <td><?php echo $dato->nombres; ?></td>
+                                        <td><?php echo $dato->apellidos; ?></td>
+                                        <td><?php echo $dato->DNI; ?></td>
+                                        <td><?php echo $dato->Tipo_habitacion; ?></td>
+                                        <td><?php echo $dato->Fecha_reserva; ?></td>
+                                        <td><?php echo $dato->hora_ingreso; ?></td>
+                                        <td><?php echo $dato->hora_salida; ?></td>
+                                        <td><?php echo $dato->celular; ?></td>
+                                        <td>
+                                            <a class="text-success" href="editar.php?codigo=<?php echo $dato->id; ?>"><i class="bi bi-pencil-square"></i></a>
+                                            <a onclick="return confirm('¿Estás seguro de eliminar?');" class="text-danger" href="eliminar.php?codigo=<?php echo $dato->id; ?>"><i class="bi bi-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'template/footer.php' ?>
---
+++ template/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>HOTEL</title>
+    <link rel="stylesheet" href="estilos.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- cdn iconos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Tus estilos CSS -->
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-gray-900 border-bottom shadow-sm mb-3">
+    <h5 class="my-0 mr-md-auto font-weight-normal text-white">HOTEL Vi</h5>
+    
+</header>
+
+
+
+</body>
+</html>
